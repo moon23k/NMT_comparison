4b197245620963f7d4e292857588d464ce4d242f
moon <d.moon23k.gmail.com>
2022-03-17 14:00:22 +0900

    vocab module has changed

--- prepare_data.sh
+++ prepare_data.sh
@@ -2,16 +2,19 @@
 mkdir -p data
 cd data
 
+
 datasets=(multi30k iwslt wmt)
 splits=(train valid test)
 tokenizers=(word bpe unigram)
 langs=(en de)
 
 
+
 #Download datasets
 for dataset in "${datasets[@]}"; do
     bash ../scripts/download_${dataset}.sh
 done
+
 
 
 #Peer Tokenize with sacremoses
@@ -26,7 +29,8 @@ for dataset in "${datasets[@]}"; do
 done
 
 
-#Build vocab
+
+#get sentencepiece
 git clone https://github.com/google/sentencepiece.git
 cd sentencepiece
 mkdir build
@@ -38,14 +42,23 @@ sudo ldconfig
 cd ../../
 
 
+
+#concat all files into one file to build vocab
+for dataset in "${datasets[@]}"; do
+    cat ${dataset}/tok/* >> ${dataset}/concat.txt
+done
+
+
+
+#Build Vocab
 for dataset in "${datasets[@]}"; do
     mkdir -p ${dataset}/vocab
     for tokenizer in "${tokenizers[@]}"; do
-        for lang in "${langs[@]}"; do
-            bash ../scripts/build_vocab.sh -i ${dataset}/tok/train.${lang} -p ${dataset}/vocab/${tokenizer}_${lang} -t ${tokenizer}
-        done
+        bash ../scripts/build_vocab.sh -i ${dataset}/concat.txt -p ${dataset}/vocab/${tokenizer} -t ${tokenizer}
     done
+    rm ${dataset}/concat.txt
 done
+
 
 
 #tokens to ids | total 54 files
@@ -55,11 +68,13 @@ for dataset in "${datasets[@]}"; do
         for tokenizer in "${tokenizers[@]}"; do
             for lang in "${langs[@]}"; do
                 mkdir -p ${dataset}/ids/${tokenizer}
-                spm_encode --model=${dataset}/vocab/${tokenizer}_${lang}.model --extra_options=bos:eos \
+                spm_encode --model=${dataset}/vocab/${tokenizer}.model --extra_options=bos:eos \
                 --output_format=id < ${dataset}/tok/${split}.${lang} > ${dataset}/ids/${tokenizer}/${split}.${lang}
             done
         done
     done
 done
 
+
+#remove sentencepiece
 rm -r sentencepiece
